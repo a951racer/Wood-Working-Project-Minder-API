1f28c7921cc6ccab8866540b7199b57b7b601831
Jon Hobbs <jon.hobbs@minnykid.com>
2024-08-22 16:15:02 -0500

    Remove AWS

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -1,17 +1,18 @@
 import multer from 'multer';
 import { loginRequired } from '../controllers/userController';
-const files = require('../controllers/fileController');
+//const files = require('../controllers/fileController');
 
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
 module.exports = function(app) {
-    app.route('/file')
+/*    app.route('/file')
         //.get(loginRequired, files.getFiles)
         .post(loginRequired, upload.single('file'), files.addNewFile);
 
     app.route('/file/:fileName')
         //.get(loginRequired, files.getFileWithID)
         //.delete(loginRequired, files.deleteFile);
+*/
 }
 
